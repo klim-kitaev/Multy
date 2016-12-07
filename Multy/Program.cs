6760707aa411d8be77949ea71a930453be33997b
klim-kitaev <klim.kitaev@gmail.com>
2016-12-07 21:50:43 +0300

    Recipe 2.3

--- Multy/Program.cs
+++ Multy/Program.cs
@@ -9,24 +9,26 @@ namespace Multy
     {
         static void Main(string[] args)
         {
-            const string MutexName = "CSharpThreadingCookbook";
-
-            using (var m=new Mutex(false,MutexName))
+            for (int i = 0; i < 6; i++)
             {
-                if (!m.WaitOne(TimeSpan.FromSeconds(5), false))
-                {
-                    WriteLine("Second instance is running!");
-                }
-                else
-                {
-                    WriteLine("Running!");
-                    ReadLine();
-                    m.ReleaseMutex();
-                }
+                string threadName = "Thread " + i;
+                int secondsToWait = 2 + 2 * i;
+
+                new Thread(() => AccessDatabase(threadName, secondsToWait)).Start();
             }
         }
 
-       
+        static SemaphoreSlim _semaphore = new SemaphoreSlim(4);
+
+        static void AccessDatabase(string name, int seconds)
+        {
+            WriteLine($"{name} waits to access a database");
+            _semaphore.Wait();
+            WriteLine($"{name} was granted an access to a database");
+            Sleep(TimeSpan.FromSeconds(seconds));
+            WriteLine($"{name} is completed");
+            _semaphore.Release();
+        }
     }
 
     
