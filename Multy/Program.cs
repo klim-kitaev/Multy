f9371804faa399cd0acb33d6373b5956dba429c2
klim-kitaev <klim.kitaev@gmail.com>
2016-12-07 21:27:13 +0300

    Recipe 2.1

--- Multy/Program.cs
+++ Multy/Program.cs
@@ -9,38 +9,73 @@ namespace Multy
     {
         static void Main(string[] args)
         {
-            var t = new Thread(FaultyThread);
-            t.Start();
-            t.Join();
-            try
+            WriteLine("Incorrect counter");
+            var c = new Counter();
+            var t1 = new Thread(() => TestCounter(c));
+            var t2 = new Thread(() => TestCounter(c));
+            var t3 = new Thread(() => TestCounter(c));
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t1.Join();
+            t2.Join();
+            t3.Join();
+            WriteLine($"Total count: {c.Count}");
+            WriteLine("--------------------------");
+            WriteLine("Correct counter");
+            var c1 = new CounterNoLock();
+            t1 = new Thread(() => TestCounter(c1));
+            t2 = new Thread(() => TestCounter(c1));
+            t3 = new Thread(() => TestCounter(c1));
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t1.Join();
+            t2.Join();
+            t3.Join();
+            WriteLine($"Total count: {c1.Count}");
+        }
+
+        static void TestCounter(CounterBase c)
+        {
+            for (int i = 0; i < 100000; i++)
             {
-                t = new Thread(BadFaultyThread);
-                t.Start();
+                c.Increment();
+                c.Decrement();
             }
-            catch (Exception ex)
+        }
+        class Counter : CounterBase
+        {
+            private int _count;
+            public int Count => _count;
+            public override void Increment()
             {
-                WriteLine("We won't get here!");
+                _count++;
+            }
+            public override void Decrement()
+            {
+                _count--;
             }
         }
 
-        static void BadFaultyThread()
+        class CounterNoLock : CounterBase
         {
-            WriteLine("Starting a faulty thread...");
-            Sleep(TimeSpan.FromSeconds(2));
-            throw new Exception("Boom!");
+            private int _count;
+            public int Count => _count;
+            public override void Increment()
+            {
+                Interlocked.Increment(ref _count);
+            }
+            public override void Decrement()
+            {
+                Interlocked.Decrement(ref _count);
+            }
         }
-        static void FaultyThread()
+
+        abstract class CounterBase
         {
-            try
-            {
-                WriteLine("Starting a faulty thread...");
-                Sleep(TimeSpan.FromSeconds(1));
-                throw new Exception("Boom!");
-            }
-            catch (Exception ex)
-            {
-                WriteLine($"Exception handled: {ex.Message}");
-            }
+            public abstract void Increment();
+            public abstract void Decrement();
         }
     }
 
