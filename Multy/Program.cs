97f176e1a16d2eef4108711f47d89abeda32f85b
Kitaev <klim.kitaev@gmail.com>
2016-12-09 17:25:58 +0300

    Recipe 3.5

--- Multy/Program.cs
+++ Multy/Program.cs
@@ -11,80 +11,23 @@ namespace Multy
     {
         static void Main(string[] args)
         {
-            using (var cts = new CancellationTokenSource())
+            WriteLine("Press 'Enter' to stop the timer...");
+            DateTime start = DateTime.Now;
+            using (_timer = new Timer(_ => TimerOperation(start), null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(2)))
             {
-                CancellationToken token = cts.Token;
-                ThreadPool.QueueUserWorkItem(_ => AsyncOperation1(token));
-                Sleep(TimeSpan.FromSeconds(2));
-                cts.Cancel();
+                Sleep(TimeSpan.FromSeconds(6));
+                _timer.Change(TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(4));
+                ReadLine();
             }
-            using (var cts = new CancellationTokenSource())
-            {
-                CancellationToken token = cts.Token;
-                ThreadPool.QueueUserWorkItem(_ => AsyncOperation2(token));
-                Sleep(TimeSpan.FromSeconds(2));
-                cts.Cancel();
-            }
-            using (var cts = new CancellationTokenSource())
-            {
-                CancellationToken token = cts.Token;
-                ThreadPool.QueueUserWorkItem(_ => AsyncOperation3(token));
-                Sleep(TimeSpan.FromSeconds(2));
-                cts.Cancel();
-            }
-            Sleep(TimeSpan.FromSeconds(2));
         }
 
-        static void AsyncOperation1(CancellationToken token)
+        static Timer _timer;
+
+        static void TimerOperation(DateTime start)
         {
-            WriteLine("Starting the first task");
-            for (int i = 0; i < 5; i++)
-            {
-                if(token.IsCancellationRequested)
-                {
-                    WriteLine("The first task has been canceled.");
-                    return;
-                }
-                Sleep(TimeSpan.FromSeconds(1));
-            }
-            WriteLine("The first task has completed succesfully");
+            TimeSpan elapsed = DateTime.Now - start;
+            WriteLine($"{elapsed.Seconds} seconds from {start}. " + $"Timer thread pool thread id: { CurrentThread.ManagedThreadId}");
         }
-
-        static void AsyncOperation2(CancellationToken token)
-        {
-            try
-            {
-                WriteLine("Starting the second task");
-                for (int i = 0; i < 5; i++)
-                {
-                    token.ThrowIfCancellationRequested();
-                    Sleep(TimeSpan.FromSeconds(1));
-                }
-                WriteLine("The second task has completed succesfully");
-            }
-            catch (OperationCanceledException)
-            {
-                WriteLine("The second task has been canceled.");
-            }
-        }
-
-        static void AsyncOperation3(CancellationToken token)
-        {
-            bool cancellationFlag = false;
-            token.Register(() => cancellationFlag = true);
-            WriteLine("Starting the third task");
-            for (int i = 0; i < 5; i++)
-            {
-                if (cancellationFlag)
-                {
-                    WriteLine("The third task has been canceled.");
-                    return;
-                }
-                Sleep(TimeSpan.FromSeconds(1));
-            }
-            WriteLine("The third task has completed succesfully");
-        }
-
 
     }
 
